ea863956257e3f7af43327eaf60f8231cbd11098
Deiter Hofstetter <dhof01@gmail.com>
2018-06-08 16:23:49 -0700

    Update group controller

--- app/controllers/groupworkout_controller.rb
+++ app/controllers/groupworkout_controller.rb
@@ -1,7 +1,7 @@
 class GroupworkoutController < ApplicationController
 
   def future
-    @future = Workout.next_year(field: :workout_date, offset: 12.hours).pluck_to_hash(:workout_name, :workout_date, :time, :location, :instructor)
+    @future = Workout.next_year(field: :workout_date).pluck_to_hash(:workout_name, :workout_date, :time, :location, :instructor)
   render json: @future
   end
 end
