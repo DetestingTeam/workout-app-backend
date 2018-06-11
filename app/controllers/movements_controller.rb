4ee8fbd949b2af9292c1511521a3d3b4790eff5d
Christine Benedict <ccbenedict3@gmail.com>
2018-06-11 14:39:20 -0700

    Made create method in Movements Controller

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -5,4 +5,18 @@ class MovementsController < ApplicationController
   render json: movements
   end
 
+  def create
+    movement = Movement.new(movement_params)
+    if movement.save
+      render json: movement, status: 201
+    else
+      render json: {errors: movement.errors}, status: 422
+    end
+  end
+
+  private
+  def movement_params
+    params.require(:movement).permit(:movement_name, :movement_description, :url, :bodypart)
+  end
+
 end
